c41da32242e5d67588cf41ba50d975cac9f2ed03
Peter Siska <psiska@inventage.com>
2020-07-27 08:48:39 +0200

    Export element name in HTMLElementTagNameMap

--- leaflet-map.ts
+++ leaflet-map.ts
@@ -1,3 +1,9 @@
 import { LeafletMap } from './src/LeafletMap.js';
 
 window.customElements.define('leaflet-map', LeafletMap);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'leaflet-map': LeafletMap;
+  }
+}
